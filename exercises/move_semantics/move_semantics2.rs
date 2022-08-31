56426204d93516b7fafe954958f91abead4dd032
Jean Cloarec <jean.cloarec@hotmail.fr>
2022-08-31 16:50:55 +0200

    second

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,7 +2,7 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let vec0 = Vec::new();
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,16 @@
 // 40 or more at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (apple_number :i32)->i32 {
+    if 40 < apple_number {
+        return apple_number
+    } else {
+        return apple_number * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
